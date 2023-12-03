1db5fe4e63515408aa61d73d73a1fd91741d0cfa
Nagydani11 <nagydani97@gmail.com>
2023-12-03 16:53:16 +0100

    Why central

---
+++ src/Public/Components/Whycentralstrength/Whycentralextras.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Whycentralextras(){
+    return(
+        <div className="extras">
+            <h2>Extras</h2>
+            <div className="extraswrap">
+                <div className="extrascard">
+                    <img />
+                    <h4>Yanga sports water</h4>
+                </div>
+                <div className="extrascard">
+                    <img />
+                    <h4>Personal coach</h4>
+                </div>
+                <div className="extrascard">
+                    <img />
+                    <h4>Training intro</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Public/Components/Whycentralstrength/Whycentralinfo.js
@@ -0,0 +1,22 @@
+import React from "react";
+import info1 from "./images/info1.jpg";
+import info2 from "./images/info2.jpeg";
+
+export default function Whycentralinfo(){
+    return(
+        <div className="whycentralinfo">
+            <h2>Relax and recover zone</h2>
+            <div className="whycentralinfowrap">
+                <img src={info1}/>
+                <p>Want to recharge so that you can give it your all during your workout? Or let your body recover from that hard work after a vigorous cardio or strength training session? Which can! Because from now on you will find new Basic-Fit massage chairs in all our clubs. Sit down, open the Basic-Fit app, put in your earphones and treat yourself to the ultimate Body & Mind experience!</p>
+            </div>
+            <a href="#">More info</a>
+            <h2>Basic fit membership</h2>
+            <div className="whycentralinfowrap">
+                <p>Want to recharge so that you can give it your all during your workout? Or let your body recover from that hard work after a vigorous cardio or strength training session? Which can! Because from now on you will find new Basic-Fit massage chairs in all our clubs. Sit down, open the Basic-Fit app, put in your earphones and treat yourself to the ultimate Body & Mind experience!</p>
+                <img src={info2}/>
+            </div>
+            <a href="#">Basic fit clubs</a>
+        </div>
+    )
+}
