926c2acb9636612c9af9c07cba93765114df7f25
Nagydani11 <nagydani97@gmail.com>
2023-12-05 21:59:29 +0100

    Header component

--- src/Public/Components/Header/Header.js
+++ src/Public/Components/Header/Header.js
@@ -1,6 +1,49 @@
 import React from "react";
+import logoimage from "../Header/logo.webp";
+import "./header.css"
+
+export default function Header() {
+
+	return (
+		<>
+			<div className="navbar">
+				<div className="logo">
+                    <img src={logoimage} />
+				</div>
+				<ul className="links">
+					<li>
+						<a href="/prices">Prices</a>
+					</li>
+					<li>
+						<a href="/whycentral">Why Central Strength</a>
+					</li>
+					<li>
+						<a href="/support">Support</a>
+					</li>
+					<li>
+						<a href="/grouplessons">Group lessons</a>
+					</li>
+                    <li>
+						<a href="/train">Train everywhere</a>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
+}
 
 
+
+
+
+
+
+
+
+
+
+
+/*
 export default function Header(){
     return(
         <div className="headerWrap">
@@ -13,3 +56,34 @@ export default function Header(){
         </div>
     )
 }
+*/
+
+/*
+export default function Header() {
+
+	return (
+		<>
+			<div className="headerWrap">
+				<div className="logo">
+                    <img src={logoimage} href="/home" />
+				</div>
+				<ul className="links">
+					<li>
+						<a href="/prices">Prices</a>
+					</li>
+					<li>
+						<a href="/whycentral">Why Central Strength</a>
+					</li>
+					<li>
+						<a href="/support">Support</a>
+					</li>
+					<li>
+						<a href="/grouplessons">Group lessons</a>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
+}
+
+*/
